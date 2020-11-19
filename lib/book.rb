a7733f36efcd2b0ac1eb193f2df7ea6d8f051a99
mccleskeyc <mccleskeyc@gmail.com>
2020-11-19 15:13:50 -0500

    commit tree 59ce7cb72b60d783cc4bb23c264433a4166decb4

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :title, :genre
+
+  GENRES = [] # sets contstant GENRES to an array because it's plural
 
   def initialize(title)
     @title = title
@@ -8,6 +10,11 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
 end
